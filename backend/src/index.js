63382f30d8db9422c68595a4e7ebb285b241d750
Neeraj Kumar <neeraj.22b1531188@abes.ac.in>
2025-03-20 04:03:46 +0530

    Resolved merge conflicts

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,8 +6,11 @@ import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 import messageRouter from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
+import path from "path";
 
 dotenv.config();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
@@ -23,12 +26,14 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRouter);
 
-app.get("/test-cookies", (req, res) => {
-  console.log("Cookies received:", req.cookies);
-  res.json({ cookies: req.cookies });
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-const PORT = process.env.PORT || 5000;
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 server.listen(PORT, () => {
   console.log("Server running on port", PORT);
   connectDB();
